a8aeb9443dd71f5ae992dbee3e996c129189b0ec
bandoto <pepperr228@gmail.com>
2023-06-05 12:51:32 +0300

    03_cli_tg_console_sender

---
+++ 03_cli_telegram_console_sender/index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import TelegramBot from 'node-telegram-bot-api';
+import { program } from "commander";
+const TELEGRAM_TOKEN = '6280016824:AAEKj2l7eNe8xVQD4su2Xww-T6EP-KAZ0Ik';
+const CHAT_ID = '341620799';
+// @ts-ignore
+process.env["NTBA_FIX_350"] = 1;
+const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
+const sendMessage = (chatId, message) => __awaiter(void 0, void 0, void 0, function* () {
+    yield bot.sendMessage(chatId, message);
+    yield process.exit();
+});
+const sendPhoto = (chatId, path) => __awaiter(void 0, void 0, void 0, function* () {
+    yield bot.sendPhoto(chatId, path);
+    yield process.exit();
+});
+const getChatId = () => __awaiter(void 0, void 0, void 0, function* () {
+    bot.onText(/\/start/, (msg) => __awaiter(void 0, void 0, void 0, function* () {
+        const chatId = msg.chat.id;
+        yield bot.sendMessage(chatId, String(chatId));
+        yield process.exit();
+    }));
+});
+const main = () => __awaiter(void 0, void 0, void 0, function* () {
+    program.command('start')
+        .description('Get current chet ID')
+        .alias('s')
+        .action(() => __awaiter(void 0, void 0, void 0, function* () {
+        yield getChatId();
+    }));
+    program.command('message')
+        .description('Send message to Telegram Bot')
+        .alias('m')
+        .argument('<message>', 'message from bot')
+        .action((message) => __awaiter(void 0, void 0, void 0, function* () {
+        yield sendMessage(CHAT_ID, message);
+    }));
+    program.command('photo')
+        .description('Send photo to Telegram Bot')
+        .alias('p')
+        .argument('<photo>', 'photo from bot')
+        .action((photo) => __awaiter(void 0, void 0, void 0, function* () {
+        yield sendPhoto(CHAT_ID, photo);
+    }));
+    yield program.parseAsync(process.argv);
+});
+main();
---
+++ 03_cli_telegram_console_sender/index.ts
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+import TelegramBot from 'node-telegram-bot-api';
+import { program } from "commander";
+
+const TELEGRAM_TOKEN = '6280016824:AAEKj2l7eNe8xVQD4su2Xww-T6EP-KAZ0Ik';
+const CHAT_ID = '341620799'; // Run "node index.js start" to get current chat id
+// @ts-ignore
+process.env["NTBA_FIX_350"] = 1;
+
+const bot = new TelegramBot(TELEGRAM_TOKEN, {polling: true});
+
+const sendMessage = async (chatId: string, message: string) => {
+    await bot.sendMessage(chatId, message);
+
+    await process.exit();
+}
+
+const sendPhoto = async (chatId: string, path: string) => {
+    await bot.sendPhoto(chatId, path);
+
+    await process.exit();
+}
+
+const getChatId = async () => {
+    bot.onText(/\/start/, async (msg) => {
+        const chatId = msg.chat.id;
+
+        await bot.sendMessage(chatId, String(chatId));
+
+        await process.exit();
+    });
+}
+
+const main = async () => {
+
+    program.command('start')
+        .description('Get current chat ID')
+        .alias('s')
+        .action(async () => {
+            await getChatId();
+        })
+
+    program.command('message')
+        .description('Send message to Telegram Bot')
+        .alias('m')
+        .argument('<message>', 'message from bot')
+        .action(async (message) => {
+            await sendMessage(CHAT_ID, message);
+        })
+
+    program.command('photo')
+        .description('Send photo to Telegram Bot')
+        .alias('p')
+        .argument('<photo>', 'photo from bot')
+        .action(async (photo) => {
+            await sendPhoto(CHAT_ID, photo)
+        })
+
+    await program.parseAsync(process.argv);
+}
+
+main();
